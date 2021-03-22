0b046268c4513fad604165031f4e0613ec5bf8da
Allen <allen@localhost>
2021-03-23 00:41:52 +0200

    v1.0.0

---
+++ src/IpApiGeoService.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Leit040\Geo;
+
+
+use  Leit040\GeoInterface\GeoIpInterface;
+use Illuminate\Support\Facades\Http;
+
+
+class IpApiGeoService implements GeoIpInterface
+{
+
+
+    public $data;
+
+
+    public function parse($ip)
+    {
+
+        $response = Http::get('http://ip-api.com/json/' . $ip . '?fields=status,continentCode,countryCode');
+        $this->data = $response->json();
+        if ($this->data['status'] == 'fail') {
+            $this->data['continentCode'] = "N/A";
+            $this->data['countryCode'] = "N/A";
+
+        }
+
+    }
+
+
+    public function continentCode()
+    {
+        return $this->data['continentCode'];
+    }
+
+    public function countryCode()
+    {
+        return $this->data['countryCode'];
+    }
+
+
+}
